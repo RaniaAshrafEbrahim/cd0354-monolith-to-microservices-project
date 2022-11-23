959d161c99e3ff481eb9598863272791e6472190
rania <rania.ashraf@goodsmartegypt.com>
2022-11-23 19:16:32 +0200

    add projects

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=rania
+export POSTGRES_PASSWORD=rania123
+export POSTGRES_HOST=d-1.clzg3ssh2xcl.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=rania-376564985474-stag
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100			
